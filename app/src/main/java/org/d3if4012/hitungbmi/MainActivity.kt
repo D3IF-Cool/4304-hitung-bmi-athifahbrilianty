8745d711b9f7f2d8cf4f835da7d3d2d45ef703ce
athifah_brilianty_nurul_falah <athifahbrilianty@gmail.com>
2021-03-17 16:34:05 +0700

    Refactor fitur hitung BMI ke fragment

--- app/src/main/java/org/d3if4012/hitungbmi/MainActivity.kt
+++ app/src/main/java/org/d3if4012/hitungbmi/MainActivity.kt
@@ -2,6 +2,9 @@ package org.d3if4012.hitungbmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import org.d3if4012.hitungbmi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
