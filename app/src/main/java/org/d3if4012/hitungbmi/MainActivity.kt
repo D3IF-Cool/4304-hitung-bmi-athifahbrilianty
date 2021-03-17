aa7110bd2da72fa7ff927aa999226e91609a1f14
athifah_brilianty_nurul_falah <athifahbrilianty@gmail.com>
2021-03-17 17:17:34 +0700

    Membuat fragment lihat saran

--- app/src/main/java/org/d3if4012/hitungbmi/MainActivity.kt
+++ app/src/main/java/org/d3if4012/hitungbmi/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Toast
-import org.d3if4012.hitungbmi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/org/d3if4012/hitungbmi/data/KategoriBmi.kt
@@ -0,0 +1,4 @@
+package org.d3if4012.hitungbmi.data
+
+enum class KategoriBmi { KURUS, IDEAL, GEMUK }
+
