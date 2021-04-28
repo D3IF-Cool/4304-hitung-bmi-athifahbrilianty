61a93ec19a5717f1d4d14ef1a7f9bbc27d37002b
athifah_brilianty_nurul_falah <athifahbrilianty@gmail.com>
2021-04-28 09:05:45 +0700

    3.3.Membuat database dengan Room

---
+++ app/src/main/java/org/d3if4012/hitungbmi/db/BmiDao.kt
@@ -0,0 +1,15 @@
+package org.d3if4012.hitungbmi.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface BmiDao {
+    @Insert
+    fun insert(bmi : BmiEntity)
+
+    @Query("SELECT * FROM bmi ORDER BY id DESC LIMIT 1")
+    fun getLastBmi() : LiveData<BmiEntity>
+}
